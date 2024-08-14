a014d420e7c1125f8835f789940f3721541c1e21
Davi Fernandes <davi.fer159@gmail.com>
2024-08-14 16:11:14 -0300

    Finish Project

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-// write your JavaScript here
+const plusButton = document.querySelectorAll(".plus");
+const exitButton = document.querySelectorAll(".exit");
+
+plusButton.forEach((plus) => {
+  plus.addEventListener("click", (e) => {
+    const target = e.target.parentNode;
+    const details = target.querySelector(".acordion-details");
+    const exit = target.querySelector(".exit");
+
+    handleButton(details, exit, plus);
+   
+  });
+});
+
+function handleButton(details, exit, plus) {
+  exit.style.display = "block";
+  details.style.display = "block";
+  plus.style.display = "none";
+
+  exitButton.forEach((exit) => {
+    exit.addEventListener("click", () => {
+      details.style.display = "none";
+      exit.style.display = "none";
+      plus.style.display = "block";
+    });
+  });
+}
